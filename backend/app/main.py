25f5a6bf9a1f0d8c24925cdb4906c0925d4047f5
jack <jackharper0517@gmail.com>
2026-09-07 18:19:29 -0500

    updated imports

--- backend/app/main.py
+++ backend/app/main.py
@@ -41,9 +41,9 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-from ..api.workflows import router as workflows_router
-from ..api.connections import router as connections_router
-from ..api.secrets import router as secrets_router
+from .api.workflows import router as workflows_router
+from .api.connections import router as connections_router
+from .api.secrets import router as secrets_router
 
 app.include_router(workflows_router, prefix=settings.api_prefix)
 app.include_router(connections_router, prefix=settings.api_prefix)
